df4b1d22e2dc8d105775fafd9baffbe6fe103278
AliSamihTarhini <114899105+AliSamihTarhini@users.noreply.github.com>
2022-12-28 10:36:00 +0200

    created character detail view

---
+++ Marvel Caracters/CharacterDetails/CharacterDetails.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterDetails.swift
+//  Marvel Caracters
+//
+//  Created by Ali Tarhini on 28/12/2022.
+//
+
+import UIKit
+
+class CharacterDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
